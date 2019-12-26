830cbc7661ae80524b9af7b529db4d060fdb2d87
it_darya_trade <it@darya-trade.ru>
2019-12-26 06:46:14 +0300

    Hello

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -9,14 +9,22 @@ public class CharacterMove : MonoBehaviour
     public float HorizontalRotationSpeed=5;
     public float VerticalRotationSpeed = 3;
     public float rotSmoothSpeed=5;
-
-    public float MaxCameraAngle=45;
+    [Range(1,5)]
+    public float Sensetivity;
+    public float MaxCameraAngle = 45;
     public float MinCameraAngle = -45;
+
+
+
+
+    private float mouseX;
+    private float mouseY;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MaxCameraAngle = -MaxCameraAngle;
+        MinCameraAngle = -MinCameraAngle;
     }
 
     // Update is called once per frame
@@ -34,29 +42,30 @@ public class CharacterMove : MonoBehaviour
 
     void RotateCharacter()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        Quaternion newRotation = new Quaternion(0, transform.rotation.y + mouseX, 0, 0);
-        transform.Rotate(new Vector3(0, mouseX*Time.deltaTime*HorizontalRotationSpeed));
+        mouseX += Input.GetAxis("Mouse X")*(Sensetivity);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.rotation.x, mouseX, 0), Time.deltaTime*rotSmoothSpeed);
     }
 
     void RotateCamera()
     {
-        
-        float mouseY = -Input.GetAxis("Mouse Y");
-        Quaternion newRotation = MainCamera.transform.rotation;
-        newRotation.x += mouseY;
-        newRotation.x = Mathf.Clamp(newRotation.x, MinCameraAngle, MaxCameraAngle);
-        //MainCamera.transform.Rotate(new Vector3(mouseY * Time.deltaTime * VerticalRotationSpeed, 0));
-        MainCamera.transform.rotation = Quaternion.Slerp(MainCamera.transform.rotation, newRotation, Time.deltaTime * rotSmoothSpeed);
+
+        mouseY += -Input.GetAxis("Mouse Y") * (Sensetivity);
+        mouseY=Mathf.Clamp(mouseY, -45f, 45f);
+        MainCamera.transform.rotation = Quaternion.Slerp(MainCamera.transform.rotation, Quaternion.Euler(mouseY, MainCamera.transform.rotation.eulerAngles.y, 0), Time.deltaTime * ((float)Screen.height / (float)Screen.width) * rotSmoothSpeed);
     }
 
     private bool isCamClamped(Quaternion rotation) {
-            Vector3 rot = rotation.eulerAngles;
-            
-            if (rot.x<=-MinCameraAngle && rot.x>=-MaxCameraAngle){
-                return true;
+        Vector3 rot = rotation.eulerAngles;
+        float angle = rot.x;
+        angle = (angle > 180) ? angle - 360 : angle;
+        Debug.Log(angle);
+        if (rot.x<=MinCameraAngle&& rot.x >= MaxCameraAngle)
+        {
+            return true;
+        }
+        else return false;
+        
 
-            }
-            else return false;
+
     }
 }
